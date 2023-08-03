4d57f86aef19e07bec2a9c75a1dcdad5fe8ed5ad
Haotian Zhu <haotzhu@amazon.com>
2023-08-03 10:36:52 -0700

    Update OpenAPIKit to aalpha.9 and fix break change

--- Sources/OpenAPIServiceModel/CreateOpenAPIServiceModel.swift
+++ Sources/OpenAPIServiceModel/CreateOpenAPIServiceModel.swift
@@ -81,7 +81,7 @@ internal extension OpenAPIServiceModel {
         for (path, pathDefinition) in definition.paths {
             var operations:[OpenAPI.HttpMethod: OpenAPI.Operation] = [:]
             
-            pathDefinition.endpoints.forEach { endpoint in
+            pathDefinition.pathItemValue?.endpoints.forEach { endpoint in
                 operations[endpoint.method] = endpoint.operation
             }
             
